e0e425b9b234d451d55c5608787a2e4e4369b7f3
David Tanzer <business@davidtanzer.net>
2018-08-14 11:10:19 +0200

    After first exercise (test red)

--- 01_intro_tdd/src/main/java/net/davidtanzer/tdd/hangman/Hangman.java
+++ 01_intro_tdd/src/main/java/net/davidtanzer/tdd/hangman/Hangman.java
@@ -1,4 +1,31 @@
 package net.davidtanzer.tdd.hangman;
 
+import java.util.List;
+import java.util.Random;
+
 public class Hangman {
+	public String chooseWord(List<String> wordsToChooseFrom) {
+
+		if(wordsToChooseFrom == null){
+			throw new IllegalArgumentException("wordstochoosefrom must not be null");
+		}
+
+		int size = wordsToChooseFrom.size();
+
+		if(size <= 0){
+			throw new IllegalArgumentException("wordstochoosefrom must not be empty");
+		}
+
+		Random random = new Random();
+
+		int index = random.nextInt(size);
+
+
+		return wordsToChooseFrom.get(index);
+	}
+
+
+	public List<Integer> findCharacters(String word, String character) {
+		return null;
+	}
 }
--- 01_intro_tdd/src/test/java/net/davidtanzer/tdd/hangman/HangmanTest.java
+++ 01_intro_tdd/src/test/java/net/davidtanzer/tdd/hangman/HangmanTest.java
@@ -2,13 +2,48 @@ package net.davidtanzer.tdd.hangman;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 class HangmanTest {
 	private Hangman hangman = new Hangman();
 
 	@Test
-	public void fixmeGiveMeABetterName() {
-		fail("Fixme: Implement me!");
+	public void testCanChooseWord() {
+
+		List<String> words = new ArrayList<String>();
+		words.add("hello");
+		words.add("word");
+		assertThat(hangman.chooseWord(words)).isNotEmpty();
+	}
+
+	@Test
+	public void testWordListMustNotBeEmpty(){
+		List<String> words = new ArrayList<String>();
+
+		assertThatThrownBy(() -> hangman.chooseWord(words))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("wordstochoosefrom must not be empty");
+	}
+
+	@Test
+	public void testWordListMustNotBeNull(){
+		assertThatThrownBy(() -> hangman.chooseWord(null))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("wordstochoosefrom must not be null");
+	}
+
+	@Test
+	public void testFindCharacters(){
+		String word = "Test";
+		String character = "e";
+
+		List<Integer> indexes = hangman.findCharacters(word, character);
+
+		assertThat(indexes).contains(1);
+
 	}
 }
