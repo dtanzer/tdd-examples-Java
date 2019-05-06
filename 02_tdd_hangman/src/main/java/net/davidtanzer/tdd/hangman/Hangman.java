0a92e806a692f16673aa6d27169d617786073ae7
David Tanzer <business@davidtanzer.net>
2019-05-06 15:56:10 +0200

    Ratezustand Happy Path fertig

--- 02_tdd_hangman/src/main/java/net/davidtanzer/tdd/hangman/Hangman.java
+++ 02_tdd_hangman/src/main/java/net/davidtanzer/tdd/hangman/Hangman.java
@@ -1,4 +1,35 @@
 package net.davidtanzer.tdd.hangman;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Hangman {
+
+
+	private final String wort;
+	private String ratezustand;
+
+	public Hangman(String wort) {
+
+		this.wort = wort;
+		ratezustand = wort.replaceAll(".", "_");
+	}
+
+	public String getRatezustand() {
+		return ratezustand;
+
+	}
+
+	public void rateBuchstabe(char buchstabe) {
+
+		int index = wort.indexOf(buchstabe);
+		StringBuilder stringBuilder = new StringBuilder(ratezustand);
+
+		while(index >= 0){
+			stringBuilder.setCharAt(index, buchstabe);
+			index = wort.indexOf(buchstabe,index+1);
+		}
+
+		ratezustand = stringBuilder.toString();
+	}
 }
--- 02_tdd_hangman/src/test/java/net/davidtanzer/tdd/hangman/HangmanTest.java
+++ 02_tdd_hangman/src/test/java/net/davidtanzer/tdd/hangman/HangmanTest.java
@@ -1,14 +1,47 @@
 package net.davidtanzer.tdd.hangman;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 class HangmanTest {
-	private Hangman hangman = new Hangman();
+	private Hangman hangman = new Hangman("test");
 
 	@Test
-	public void fixmeGiveMeABetterName() {
-		fail("Fixme: Implement me!");
+	public void platzHalterStimmtMitWortlaenge() {
+		assertThat(hangman.getRatezustand().length()).isEqualTo(4);
 	}
+
+	@Test
+	public void initialPlatzhalter(){
+		assertThat(hangman.getRatezustand()).matches("_+");
+	}
+
+	@Test
+	public void platzHalterStimmtMitWortlaenge_keineAhnung() {
+		Hangman hangman = new Hangman("keineAhnung");
+		assertThat(hangman.getRatezustand().length()).isEqualTo(11);
+	}
+
+	@Test
+	public void ungeratenerBuchstabeWirdNichtAufgedeckt( ){
+		hangman.rateBuchstabe('e');
+		assertThat(hangman.getRatezustand()).isEqualTo("_e__");
+	}
+
+	@Test
+	public void mehrereBuchstabenWerdenAufgedeckt() {
+		hangman.rateBuchstabe('s');
+		hangman.rateBuchstabe('e');
+		assertThat(hangman.getRatezustand()).isEqualTo("_es_");
+	}
+
+	@Test
+	public void alleGleichenBuchstabenWerdenAufgedeckt(){
+		hangman.rateBuchstabe('t');
+		assertThat(hangman.getRatezustand()).isEqualTo("t__t");
+	}
+
+
 }
